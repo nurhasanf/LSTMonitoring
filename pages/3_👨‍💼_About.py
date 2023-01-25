05c7dbac0256e0cf85160626c8e410b6e17e4369
nurhasanf <idoandify16@gmail.com>
2023-01-25 20:19:24 +0700

    Merubah page about

--- pages/3_👨‍💼_About.py
+++ pages/3_👨‍💼_About.py
@@ -1,18 +1,2 @@
 import streamlit as st
 
-# Display header.
-st.markdown("<br>", unsafe_allow_html=True)
-st.markdown("""
-    # Land Surface Temperature - River Basins
-    [![Follow](https://img.shields.io/twitter/follow/mykolakozyr?style=social)](https://www.twitter.com/mykolakozyr)
-    [![Follow](https://img.shields.io/badge/LinkedIn-blue?style=flat&logo=linkedin&labelColor=blue)](https://www.linkedin.com/in/mykolakozyr/)
-    
-    ## Details
-
-    The app enables discovering land surface temperature data over river basins in Europe. Temporal extent: 2017-01-01 till today.
-    Library for visualizations - [Vega-Altair](https://altair-viz.github.io/index.html).
-
-    ---
-    104.17.2.81
-    443
-    """)
